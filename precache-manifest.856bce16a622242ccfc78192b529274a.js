983bbe14c384705c93ca41c5e537e99abd260fda
Sebastian Mocanu <diemetal851@gmail.com>
2020-09-23 04:21:15 +0300

    Updates

---
+++ precache-manifest.856bce16a622242ccfc78192b529274a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f6a3097d69948db165e5afe336beb5",
+    "url": "/react-javascript-calculator/index.html"
+  },
+  {
+    "revision": "ed4c9014fc1e45c636f8",
+    "url": "/react-javascript-calculator/static/css/main.66760fa6.chunk.css"
+  },
+  {
+    "revision": "a50be34fe3e334a2adb7",
+    "url": "/react-javascript-calculator/static/js/2.f7cd2583.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-javascript-calculator/static/js/2.f7cd2583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4c9014fc1e45c636f8",
+    "url": "/react-javascript-calculator/static/js/main.c0f62737.chunk.js"
+  },
+  {
+    "revision": "be6256bc11f35cec7336",
+    "url": "/react-javascript-calculator/static/js/runtime-main.cd9be910.js"
+  }
+]);
